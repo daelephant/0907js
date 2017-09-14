882d91591f3ee8e81abb1cc61aeac1375e228f59
elephant2 <2224168716@qq.com>
2017-09-14 16:06:12 +0800

    Merge branch 'master' of https://github.com/daelephant/0907js
    # Conflicts:
    #	ajax0916/test/page/data.php
    #	ajax0916/test/page/index.html
    #	ajax0916/test/page/page.class.php

--- ajax0916/test/uploadForm/06.php
+++ ajax0916/test/uploadForm/06.php
@@ -1,22 +1,49 @@
 <?php
-//收集到：普通表单域信息 +  上传文件域信息
-echo "post:";
-print_r($_POST);
-echo "file:";
-print_r($_FILES);
+
+//收集到：普通表单域信息 + 上传文件域信息
+//print_r($_POST);
+//echo "files:";
+//print_r($_FILES);exit;
+
 if($_FILES['userpic']['error']>0){
     exit('附件有错误');
 }
+
 //附件上传逻辑
 $path = "./upload/";
 $name = date("YmdHis").'-'.mt_rand(1000,9999);//附件的名字
-$name_arr =  explode('.',$_FILES['userpic']['name']);
-$ext = ".".$name_arr[count($name_arr)-1]; //附件的后缀
+$name_arr = explode('.',$_FILES['userpic']['name']);//获得照片原名称和后缀的数组{[picname][png]}
+$ext = ".".$name_arr[count($name_arr)-1];//获得附件后缀 （.png）
 
-$pathname = $path.$name.$ext;//附件真实路径名
+$pathname = $path.$name.$ext;//附件真实路径名(这里的点是拼接)
 
-if(move_uploaded_file($_FILES['userpic']['tmp_name'],$pathname)){
+if(move_uploaded_file($_FILES['userpic']['tmp_name'],$pathname)){//move_uploaded_file(附件临时路径名，真实附件路径名);
     echo "success";
 }else{
     echo "fail";
 }
+
+//var_dump($pathname);
+
+
+////收集到：普通表单域信息 +  上传文件域信息
+//echo "post:";
+//print_r($_POST);
+//echo "file:";
+//print_r($_FILES);
+//if($_FILES['userpic']['error']>0){
+//    exit('附件有错误');
+//}
+////附件上传逻辑
+//$path = "./upload/";
+//$name = date("YmdHis").'-'.mt_rand(1000,9999);//附件的名字
+//$name_arr =  explode('.',$_FILES['userpic']['name']);
+//$ext = ".".$name_arr[count($name_arr)-1]; //附件的后缀
+//
+//$pathname = $path.$name.$ext;//附件真实路径名
+//
+//if(move_uploaded_file($_FILES['userpic']['tmp_name'],$pathname)){
+//    echo "success";
+//}else{
+//    echo "fail";
+//}
